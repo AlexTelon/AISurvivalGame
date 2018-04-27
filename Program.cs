87f11619f24209990b7e9deb236a65af89de781b
Alex Telon <alex.telon@gmail.com>
2018-04-27 22:13:57 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SurvivalAIGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
