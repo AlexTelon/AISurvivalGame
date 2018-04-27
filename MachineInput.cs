0cd1685475dd1a3d35e4f093d31c0e92bbb8eb4a
Alex Telon <alex.telon@gmail.com>
2018-04-27 23:19:34 +0200

    Added a random agent that plays the game

---
+++ MachineInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SurvivalAIGame
+{
+    class MachineInput : IInput
+    {
+        private static Random Rnd = new Random();
+
+        public int Get()
+        {
+            // let the machine anwer questions according to some rule
+            return Rnd.Next(1, 3);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace SurvivalAIGame
         static void Main(string[] args)
         {
             // start a game and give it a input device
-            var game = new Game(new UserInput());
+            //var game = new Game(new UserInput());
+            var game = new Game(new MachineInput());
 
             Console.WriteLine("Starting game..");
 
